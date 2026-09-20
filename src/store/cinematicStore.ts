40517b56ef04d53a65fe88c92997fa02f3b8e6db
hustleglobal95 <276327368+hustleglobal95@users.noreply.github.com>
2026-09-20 07:51:24 -0400

    Track cursor reveal pointer state

--- src/store/cinematicStore.ts
+++ src/store/cinematicStore.ts
@@ -1,5 +1,6 @@
 "use client";
 import { create } from "zustand";
 export interface PointerTrailPoint{x:number;y:number;age:number}
-interface CinematicState{springProgress:number;springVelocity:number;pointer:{x:number;y:number;velocityX:number;velocityY:number;speed:number;pressure:number;dwell:number;trailEnergy:number};trail:PointerTrailPoint[];setSpring:(value:number,velocity:number)=>void;setPointerSignal:(value:CinematicState["pointer"],trail:PointerTrailPoint[])=>void;}
-export const useCinematicStore=create<CinematicState>((set)=>({springProgress:0,springVelocity:0,pointer:{x:0,y:0,velocityX:0,velocityY:0,speed:0,pressure:0,dwell:0,trailEnergy:0},trail:[],setSpring:(springProgress,springVelocity)=>set({springProgress,springVelocity}),setPointerSignal:(pointer,trail)=>set({pointer,trail})}));
+export interface CinematicPointerSignal{x:number;y:number;velocityX:number;velocityY:number;speed:number;pressure:number;dwell:number;trailEnergy:number;pointerType:string;down:boolean;active:boolean;lastMoveAt:number}
+interface CinematicState{springProgress:number;springVelocity:number;pointer:CinematicPointerSignal;trail:PointerTrailPoint[];setSpring:(value:number,velocity:number)=>void;setPointerSignal:(value:CinematicPointerSignal,trail:PointerTrailPoint[])=>void;}
+export const useCinematicStore=create<CinematicState>((set)=>({springProgress:0,springVelocity:0,pointer:{x:0,y:0,velocityX:0,velocityY:0,speed:0,pressure:0,dwell:0,trailEnergy:0,pointerType:"mouse",down:false,active:false,lastMoveAt:0},trail:[],setSpring:(springProgress,springVelocity)=>set({springProgress,springVelocity}),setPointerSignal:(pointer,trail)=>set({pointer,trail})}));
